5623d1fb76dc6820a9d17fdd3286e267aa4ebb99
skorpion <jmosinski@o2.pl>
2025-11-11 17:31:48 +0100

    added font

--- src/Components/LottoHeader/LottoHeader.tsx
+++ src/Components/LottoHeader/LottoHeader.tsx
@@ -1,7 +1,8 @@
 const LottoHeader = () => {
 	return (
 		<section className='w-full h-[10%] flex justify-center items-center'>
-			<h1 className='text-amber-50 md:text-amber-200 lg:text-amber-600'>Tekst</h1>
+			<h1 className='text-amber-50 md:text-amber-200 lg:text-amber-600'>{`losuj `}</h1>
+			<span></span>
 		</section>
 	)
 }
