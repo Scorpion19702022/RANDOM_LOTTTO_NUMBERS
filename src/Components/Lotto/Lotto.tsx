771f21ba46b92b77907b426445ccb42675bed086
skorpion <jmosinski@o2.pl>
2025-11-11 16:38:16 +0100

    added component

--- src/Components/Lotto/Lotto.tsx
+++ src/Components/Lotto/Lotto.tsx
@@ -1,6 +1,6 @@
 const Lotto = () => {
 	return (
-		<main className='w-full h-screen flex text-[5em]'>
+		<main className='w-full h-screen flex flex-col justify-around items-center'>
 			<h1>Tekst</h1>
 		</main>
 	)
