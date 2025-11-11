3fc8c178928a2a1641e6c2a8cba5622d8156fb3f
skorpion <jmosinski@o2.pl>
2025-11-11 16:56:55 +0100

    added style

--- src/Components/LottoHeader/LottoHeader.tsx
+++ src/Components/LottoHeader/LottoHeader.tsx
@@ -1,7 +1,7 @@
 const LottoHeader = () => {
 	return (
 		<section className='w-full h-[10%] flex justify-center items-center'>
-			<h1>Tekst</h1>
+			<h1 className='text-amber-50 md:text-amber-200 lg:text-amber-600'>Tekst</h1>
 		</section>
 	)
 }
