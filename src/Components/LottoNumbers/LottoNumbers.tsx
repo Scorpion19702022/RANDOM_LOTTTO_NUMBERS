4c7e650937da9840fd832539e876738b4385253d
skorpion <jmosinski@o2.pl>
2025-11-12 22:03:09 +0100

    added component

---
+++ src/Components/LottoNumbers/LottoNumbers.tsx
@@ -0,0 +1,12 @@
+const LottoNumbers = () => {
+	return (
+		<section>
+			<div>
+				<button>{`duży lotek`.toLocaleUpperCase()}</button>
+				<button>{`duży lotek`.toLocaleUpperCase()}</button>
+			</div>
+		</section>
+	)
+}
+
+export default LottoNumbers
