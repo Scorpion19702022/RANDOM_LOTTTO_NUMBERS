b453f6ff6118b8d707ed2dd083e82d3d9510256f
skorpion <jmosinski@o2.pl>
2025-11-12 22:08:56 +0100

    added component

--- src/Components/Lotto/Lotto.tsx
+++ src/Components/Lotto/Lotto.tsx
@@ -1,9 +1,11 @@
 import LottoHeader from '../LottoHeader/LottoHeader'
+import LottoNumbers from '../LottoNumbers/LottoNumbers'
 
 const Lotto = () => {
 	return (
 		<main className='w-full h-screen bg-gray-700 flex flex-col items-center'>
 			<LottoHeader />
+			<LottoNumbers />
 		</main>
 	)
 }
