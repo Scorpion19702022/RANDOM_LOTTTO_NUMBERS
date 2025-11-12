592b23736af9e587f657497d9553c1a4dc00cbf6
skorpion <jmosinski@o2.pl>
2025-11-12 22:08:03 +0100

    added style

--- src/Components/LottoNumbers/LottoNumbers.tsx
+++ src/Components/LottoNumbers/LottoNumbers.tsx
@@ -1,9 +1,9 @@
 const LottoNumbers = () => {
 	return (
-		<section className='w-[90%] h-[30%] flex flex-col justify-between p-6'>
-			<div>
-				<button>{`duży lotek`.toLocaleUpperCase()}</button>
-				<button>{`duży lotek`.toLocaleUpperCase()}</button>
+		<section className='w-[90%] h-[30%] flex flex-col justify-between p-6 bg-amber-400'>
+			<div className='w-full h-[20%]'>
+				<button className=''>{`duży lotek`.toLocaleUpperCase()}</button>
+				<button className=''>{`duży lotek`.toLocaleUpperCase()}</button>
 			</div>
 		</section>
 	)
