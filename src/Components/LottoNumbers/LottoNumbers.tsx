41dc73c29e7b181d6fbb2e54af244eb142efc088
skorpion <jmosinski@o2.pl>
2025-11-13 21:48:41 +0100

    added style

--- src/Components/LottoNumbers/LottoNumbers.tsx
+++ src/Components/LottoNumbers/LottoNumbers.tsx
@@ -1,15 +1,15 @@
 const LottoNumbers = () => {
 	return (
-		<section className='w-[90%] h-[30%] flex flex-col justify-around items-center p-6 border-b-2 border-gray-400'>
-			<div className='w-full h-[20%] flex justify-between md:w-[50%]'>
-				<button className='w-[40%] border-2 border-gray-200 text-gray-200 cursor-pointer transition duration-300 md:w-[40%] lg:w-[45%] hover:bg-gray-800'>
+		<section className='w-[90%] h-[35%] flex flex-col justify-between items-center p-[2em] border-b-2 bg-gray-300 border-gray-400'>
+			<div className='w-full h-[25%] flex justify-between md:w-[50%]'>
+				<button className='w-[40%] h-[70%] border-2 border-gray-200 text-gray-200 cursor-pointer transition duration-300 md:w-[40%] lg:w-[45%] hover:bg-gray-800'>
 					{`duży lotek`.toLocaleUpperCase()}
 				</button>
-				<button className='w-[40%] border-2 border-gray-200 text-gray-200 cursor-pointer transition duration-300 md:w-[40%] lg:w-[45%] hover:bg-gray-800'>
+				<button className='w-[40%] h-[70%] border-2 border-gray-200 text-gray-200 cursor-pointer transition duration-300 md:w-[40%] lg:w-[45%] hover:bg-gray-800'>
 					{`express lotek`.toLocaleUpperCase()}
 				</button>
 			</div>
-			<div>
+			<div className='w-full h-[65%] flex justify-center items-center'>
 				<span>1, 2, 3, 4, 5, 6</span>
 			</div>
 		</section>
