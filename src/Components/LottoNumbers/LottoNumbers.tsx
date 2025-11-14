cad440b443e6ed69fd894e957046bf1fd940542f
skorpion <jmosinski@o2.pl>
2025-11-14 20:37:23 +0100

    added style

--- src/Components/LottoNumbers/LottoNumbers.tsx
+++ src/Components/LottoNumbers/LottoNumbers.tsx
@@ -1,8 +1,10 @@
 const LottoNumbers = () => {
 	return (
 		<section className='w-[90%] h-[35%] flex flex-col justify-between items-center p-[2em] border-b-2 border-gray-400'>
-			<h2>wybierz losowanie klikając w odpowiedni przycisk i odkryj cyfry</h2>
-			<div className='w-full h-[30%] flex justify-between md:w-[50%]'>
+			<h2 className='w-full h-[30%] text-center text-cyan-200'>
+				wybierz losowanie klikając w odpowiedni przycisk i odkryj cyfry
+			</h2>
+			<div className='w-full h-[17%] flex justify-between md:w-[50%]'>
 				<button className='w-[40%] border-2 border-gray-200 text-gray-200 cursor-pointer transition duration-300 md:w-[40%] lg:w-[45%] hover:bg-gray-800'>
 					{`duży lotek`.toLocaleUpperCase()}
 				</button>
@@ -10,7 +12,7 @@ const LottoNumbers = () => {
 					{`express lotek`.toLocaleUpperCase()}
 				</button>
 			</div>
-			<div className='w-full h-[55%] flex justify-center items-center'>
+			<div className='w-full h-[45%] flex justify-center items-center'>
 				<span>1, 2, 3, 4, 5, 6</span>
 			</div>
 		</section>
