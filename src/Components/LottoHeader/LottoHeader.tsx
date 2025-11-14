c2103eca089bc2766d1d76998df4c4678d33441f
skorpion <jmosinski@o2.pl>
2025-11-14 20:43:29 +0100

    added style

--- src/Components/LottoHeader/LottoHeader.tsx
+++ src/Components/LottoHeader/LottoHeader.tsx
@@ -1,7 +1,7 @@
 const LottoHeader = () => {
 	return (
 		<section className='w-[90%] h-[10%] p-12 flex justify-center items-center border-b-4 border-gray-200'>
-			<h1 className='text-[1.2em] text-gray-200 text-center md:text-[1.6em] lg:text-[2.4em]'>
+			<h1 className='text-[1.2em] text-gray-200 text-center md:text-[1.4em] lg:text-[2em]'>
 				{`wylosuj swoje szczęśliwe liczby i wygraj fortunę`.toLocaleUpperCase()}
 			</h1>
 		</section>
