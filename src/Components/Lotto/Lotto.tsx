076441cfce333bd050d9132faa819ef454b70381
skorpion <jmosinski@o2.pl>
2025-11-15 17:58:47 +0100

    added component

--- src/Components/Lotto/Lotto.tsx
+++ src/Components/Lotto/Lotto.tsx
@@ -1,11 +1,13 @@
 import LottoHeader from '../LottoHeader/LottoHeader'
 import LottoNumbers from '../LottoNumbers/LottoNumbers'
+import LottoRandomNumbers from '../LottoRundomNumbers/LottoRandomNumbers'
 
 const Lotto = () => {
 	return (
 		<main className='w-full h-screen bg-gray-700 flex flex-col items-center'>
 			<LottoHeader />
 			<LottoNumbers />
+			<LottoRandomNumbers />
 		</main>
 	)
 }
---
+++ src/Components/LottoRundomNumbers/LottoRandomNumbers.tsx
@@ -0,0 +1,16 @@
+const LottoRandomNumbers = () => {
+	return (
+		<section>
+			<h1>{'twoje szczęśliwe liczby'}</h1>
+			<div>
+				<button>losuj</button>
+				<button>wyczyść</button>
+			</div>
+			<div>
+				<span>1, 2, 3, 4, 5, 6, 7</span>
+			</div>
+		</section>
+	)
+}
+
+export default LottoRandomNumbers
