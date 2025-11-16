0e333b4fb7ea8abd686cb442dfc876c19d46d017
skorpion <jmosinski@o2.pl>
2025-11-16 17:04:05 +0100

    added state

--- src/Components/Context/LottoContext.tsx
+++ src/Components/Context/LottoContext.tsx
@@ -7,22 +7,25 @@ type ContextProviderProps = {
 
 type InitialStateType = {
 	bigLottoNumbers: number[]
+	expressLottoNumbers: number[]
 }
 
 const InitialState: InitialStateType = {
 	bigLottoNumbers: [],
+	expressLottoNumbers: [],
 }
 
 const LottoContext = createContext(InitialState)
 
 export const ContextProvider = ({ children }: ContextProviderProps) => {
 	const [bigLottoNumbers, setBigLottoNumbers] = useState<number[]>([])
+	const [expressLottoNumbers, setExpressLottoNumbers] = useState<number[]>([])
 
 	// setBigLottoNumbers([1, 2])
 	console.log(bigLottoNumbers)
 
 	return (
-		<LottoContext.Provider value={{ bigLottoNumbers }}>
+		<LottoContext.Provider value={{ bigLottoNumbers, expressLottoNumbers }}>
 			{children}
 			<Analytics />
 		</LottoContext.Provider>
