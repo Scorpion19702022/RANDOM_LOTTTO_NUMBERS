21c0cf38545c74f7df8cf5e07066baa7d7d630f3
skorpion <jmosinski@o2.pl>
2025-11-16 17:11:09 +0100

    added props

--- src/Components/LottoNumbers/LottoNumbers.tsx
+++ src/Components/LottoNumbers/LottoNumbers.tsx
@@ -1,14 +1,24 @@
-const LottoNumbers = () => {
+interface InitialProps {
+	viewNumbers: (kind: string) => void
+}
+
+const LottoNumbers: React.FC<InitialProps> = ({ viewNumbers }) => {
 	return (
 		<section className='w-[90%] h-[35%] flex flex-col justify-between items-center p-[2em] border-b-2 border-gray-400'>
 			<h2 className='w-full h-[30%] flex items-center justify-center text-center text-cyan-200 md:text-[1.4em]'>
 				wybierz losowanie klikając w odpowiedni przycisk i odkryj cyfry
 			</h2>
 			<div className='w-full h-[17%] flex justify-between md:w-[50%]'>
-				<button className='w-[40%] border-2 border-gray-200 text-gray-200 cursor-pointer transition duration-300 md:w-[40%] lg:w-[45%] hover:bg-gray-800'>
+				<button
+					onClick={() => viewNumbers('bigLotto')}
+					className='w-[40%] border-2 border-gray-200 text-gray-200 cursor-pointer transition duration-300 md:w-[40%] lg:w-[45%] hover:bg-gray-800'
+				>
 					{`duży lotek`.toLocaleUpperCase()}
 				</button>
-				<button className='w-[40%] border-2 border-gray-200 text-gray-200 cursor-pointer transition duration-300 md:w-[40%] lg:w-[45%] hover:bg-gray-800'>
+				<button
+					onClick={() => viewNumbers('expressLotto')}
+					className='w-[40%] border-2 border-gray-200 text-gray-200 cursor-pointer transition duration-300 md:w-[40%] lg:w-[45%] hover:bg-gray-800'
+				>
 					{`express lotek`.toLocaleUpperCase()}
 				</button>
 			</div>
