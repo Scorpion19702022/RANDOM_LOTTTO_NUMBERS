6bf41e954060aca1f1e1314c4aa126c52b05c9c8
skorpion <jmosinski@o2.pl>
2025-11-20 20:43:42 +0100

    added props

--- src/Components/LottoRundomNumbers/LottoRandomNumbers.tsx
+++ src/Components/LottoRundomNumbers/LottoRandomNumbers.tsx
@@ -1,11 +1,18 @@
-const LottoRandomNumbers = () => {
+interface InitialProps {
+	randomNumbers: () => void
+}
+
+const LottoRandomNumbers: React.FC<InitialProps> = ({ randomNumbers }) => {
 	return (
 		<section className='w-[90%] h-[35%] p-[2em] flex flex-col justify-around items-center md:h-[50%] md:justify-between md:pb-24'>
 			<h1 className='w-full pb-2 text-center text-gray-300 border-b-2 border-dotted border-b-gray-500 md:w-[50%] md:text-[1.6em]'>
 				{'twoje szczęśliwe liczby'.toLocaleUpperCase()}
 			</h1>
 			<div className='w-full h-[20%] flex justify-between items-center md:w-[50%] md:h-[11%]'>
-				<button className='w-[40%] h-full border-2 border-gray-200 text-gray-200 cursor-pointer transition duration-300 md:w-[40%] lg:w-[45%] hover:bg-gray-800'>
+				<button
+					onClick={randomNumbers}
+					className='w-[40%] h-full border-2 border-gray-200 text-gray-200 cursor-pointer transition duration-300 md:w-[40%] lg:w-[45%] hover:bg-gray-800'
+				>
 					{`losuj`.toLocaleUpperCase()}
 				</button>
 				<button className='w-[40%] h-full border-2 border-gray-200 text-gray-200 cursor-pointer transition duration-300 md:w-[40%] lg:w-[45%] hover:bg-gray-800'>
