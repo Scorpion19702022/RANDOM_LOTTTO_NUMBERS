26d095f6c6f15cf0396cda9d3e9018fa8ce8c190
skorpion <jmosinski@o2.pl>
2025-11-24 21:03:13 +0100

    added style

--- src/Components/LottoPopupError/LottoPopupError.tsx
+++ src/Components/LottoPopupError/LottoPopupError.tsx
@@ -1,7 +1,7 @@
 const LottoPopupError = () => {
 	return (
-		<section className='absolute w-[70%] h-[50%] bg-gray-950'>
-			<h1>popup</h1>
+		<section className='absolute w-[70%] h-[50%] bg-gray-950 flex justify-center items-center'>
+			<h1 className='text-gray-100'>popup</h1>
 		</section>
 	)
 }
