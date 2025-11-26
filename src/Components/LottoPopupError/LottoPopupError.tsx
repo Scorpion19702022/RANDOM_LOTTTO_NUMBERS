59bcb9448aa643ecd2d9444b0510f18857949b3f
skorpion <jmosinski@o2.pl>
2025-11-26 21:06:51 +0100

    added style

--- src/Components/LottoPopupError/LottoPopupError.tsx
+++ src/Components/LottoPopupError/LottoPopupError.tsx
@@ -1,6 +1,6 @@
 const LottoPopupError = () => {
 	return (
-		<section className='absolute w-full h-[70%] bg-gray-950 border-[0.04em] border-gray-100 flex justify-center items-center shadow-[0px_0px_12px_3px_cyan] lg:w-[70%] visible'>
+		<section className='absolute w-full h-[70%] bg-gray-950 border-[0.04em] border-gray-100 flex justify-center items-center shadow-[0px_0px_12px_3px_cyan] lg:w-[70%] invisible'>
 			<h1 className='text-gray-100 text-[1.4em] p-[2em] text-center md:text-[1.8em] lg:text-[2.8em]'>
 				{`musisz wybrać losowanie i odkryć liczby!!!`.toLocaleUpperCase()}
 			</h1>
